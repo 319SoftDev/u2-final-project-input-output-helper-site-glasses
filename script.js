99b5cd6198b7c22cd2745318a24211b820e640c7
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-19 09:12:38 -0500

    update stylesheet and javascript

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// variables in the site
+const YourNameInput = document.querySelector("#your-name");
+const YourAgeInput = document.querySelector("#your-age");
+const CrushNameInput = document.querySelector("#crush-name");
+const CrushAgeInput = document.querySelector("#crush-age");
 
