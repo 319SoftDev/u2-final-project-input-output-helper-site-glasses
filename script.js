6f38225e81cc2cad16847034fbc234ee7cf88a52
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-19 09:38:56 -0500

    update html and javascript

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-// variables in the site
-const YourNameInput = document.querySelector("#your-name");
-const YourAgeInput = document.querySelector("#your-age");
-const CrushNameInput = document.querySelector("#crush-name");
-const CrushAgeInput = document.querySelector("#crush-age");
+// name input functions
+const calculateButton = document.querySelector("#calculate-button");
 
+const YourNameInput = document.querySelector("#your-name");
+const CrushNameInput = document.querySelector("#crush-name");
+
+// age input functions
+const YourAgeInput = document.querySelector("#your-age");
+const CrushAgeInput = document.querySelector("#crush-age");
+const resultsDiv = document.querySelector("#results")
+
+const ageCompatibility = () => {
+    if (YourAgeInput < CrushAgeInput){
+        youngerRange = 2 * (YourAgeInput - 7);
+        console.log(youngerRange);
+        if (CrushAgeInput > youngerRange){
+            resultsDiv.innerHTML("Too old!");
+        } else {
+            resultsDiv.innerHTML("Just enough!");
+        }
+    } else if (YourAgeInput > CrushAgeInput) {
+        olderRange = (YourAgeInput/2) - 7;
+        console.log(olderRange);
+        if (CrushAgeInput < olderRange) {
+            resultsDiv.innerHTML("Woah you creep too young...");
+        } else {
+            resultsDiv.innerHTML("You're not too old for them ;)");
+        }
+    }
+}
+
+calculateButton.addEventListener('click', ageCompatibility);
