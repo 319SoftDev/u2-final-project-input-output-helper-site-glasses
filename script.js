e6de94ba7f1cd75e06677ad0d1c31f730ea3798a
JimenaPerez22 <jimenaperezmorales12@gmail.com>
2024-11-20 10:14:15 -0500

    Updated JAVA

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // name input functions
 const calculateButton = document.querySelector("#calculate-button");
+const yourNameInput = document.querySelector("#your-name");
+const crushNameInput = document.querySelector("#crush-name");
+
+const set1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+const set2 = ["j", "k", "l", "m", "n", "o", "p", "q", "r"];
+const set3 = ["s", "t", "u", "v", "w", "x", "y", "z"];
+
+const nameCompatibility = () => {
+
+}
 
 // age input functions
 const YourAgeInput = document.querySelector("#your-age");
 const CrushAgeInput = document.querySelector("#crush-age");
-const resultsDiv = document.querySelector("#results");
+const resultsDiv = document.querySelector("#ageResults");
 
 const ageCompatibility = () => {
     // Get the actual numerical values from the inputs
@@ -34,7 +44,7 @@ const ageCompatibility = () => {
         if (crushAge < olderRange) {
             resultsDiv.innerHTML = "Woah you creep too young...";
         } else {
-            resultsDiv.innerHTML = "You're not too old for them ;)";
+            resultsDiv.innerHTML = "You're not too old for them (;";
         }
     } else {
         resultsDiv.innerHTML = "You are the same age!";
@@ -42,3 +52,36 @@ const ageCompatibility = () => {
 }
 
 calculateButton.addEventListener('click', ageCompatibility);
+
+// final results
+const percentage = document.querySelector("#percentage");
+const phrase = document.querySelector("phrase");
+
+const finalResults = () => {
+    if (percentage < 11) {
+        phrase.innerHTML = "L, GIVE UP !!! it’s never going to happen!";
+
+    } else if (10 < percentage < 21) {
+        phrase.innerHTML = "Find someone else they are not for u!";
+
+    } else if (20 < percentage < 31) {
+        phrase.innerHTML = "You can try but I doubt it will happen";
+
+    } else if (30 < percentage < 41) {
+        phrase.innerHTML = "Is your bank account cute? If no then no chance, if yes then maybe";
+
+    } else if (40 < percentage < 51) {
+        phrase.innerHTML = "You're going to be the joke  of the friend group";
+
+    } else if (50 < percentage < 61) {
+
+    } else if (60 < percentage < 71) {
+
+    } else if (70 < percentage < 81) {
+
+    } else if (80 < percentage < 91) {
+
+    } else if (90 < percentage < 101) {
+
+    }
+}
