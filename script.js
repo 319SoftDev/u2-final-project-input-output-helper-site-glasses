1cae5e9853e135c2980039e057713bd573997533
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-21 09:37:52 -0500

    update javascript

--- script.js
+++ script.js
@@ -4,40 +4,17 @@ const yourNameInput = document.querySelector("#your-name");
 const crushNameInput = document.querySelector("#crush-name");
 const percentage = document.querySelector("#percentage");
 
-function getNameCompatibility(name1, name2) {
-    function nameToNumber(name) {
-        let sum = 0;
-        name = name.toUpperCase();
-        for (let i = 0; i < name.length; i++) {
-            const char = name[i];
-            if (char >= 'A' && char <= 'Z') {
-                sum += char.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
-            }
-        }
-        return sum;
-    }
-
-    // Convert both names to number sums
-    const sum1 = nameToNumber(name1);
-    const sum2 = nameToNumber(name2);
-
-    const compatibilityScore = (sum1 + sum2) % 100;
-    percentage.innerHTML = `${compatibilityScore}`;
-    return compatibilityScore;
-}
-
-const name1 = yourNameInput;
-const name2 = crushNameInput;
-const compatibility = getNameCompatibility(name1, name2);
-console.log(`The compatibility score between ${name1} and ${name2} is: ${compatibility}`);
-
-
 // age input functions
 const YourAgeInput = document.querySelector("#your-age");
 const CrushAgeInput = document.querySelector("#crush-age");
 const ageResult = document.querySelector("#ageResults");
 
-const ageCompatibility = () => {
+// final results
+const phrase = document.querySelector("#phrase")
+
+
+const compatibility = () => {
+    // compatibility with age
     const yourAge = parseInt(YourAgeInput.value);
     const crushAge = parseInt(CrushAgeInput.value);
 
@@ -68,42 +45,56 @@ const ageCompatibility = () => {
     } else {
         ageResult.innerHTML = "You are the same age!";
     }
-}
 
-calculateButton.addEventListener('click', ageCompatibility);
+    // compatibility with names
+    function getRandomInt() {
+        return Math.floor(Math.random() * 100);
+      }
+    percentage.innerHTML = `${getRandomInt()}%`;
 
-// final results
-const finalResults = () => {
-    if (percentage < 11) {
-        phrase.innerHTML = "L, GIVE UP !!! it’s never going to happen!";
-
-    } else if (10 < percentage < 21) {
-        phrase.innerHTML = "Find someone else they are not for u!";
-
-    } else if (20 < percentage < 31) {
-        phrase.innerHTML = "You can try but I doubt it will happen";
-
-    } else if (30 < percentage < 41) {
-        phrase.innerHTML = "Is your bank account cute? If no then no chance, if yes then maybe";
-
-    } else if (40 < percentage < 51) {
-        phrase.innerHTML = "You're going to be the joke  of the friend group";
-
-    } else if (50 < percentage < 61) {
-        phrase.innerHTML = "sure...";
-
-    } else if (60 < percentage < 71) {
-        phrase.innerHTML = "If you're not scared of rejection, go for it but don't blame me if they don't accept";
-
-    } else if (70 < percentage < 81) {
-        phrase.innerHTML = "Shoot your shot";
-
-    } else if (80 < percentage < 91) {
-        phrase.innerHTML = "You better tell them";
-
-    } else if (90 < percentage < 101) {
-        phrase.innerHTML = "Invite me to the carne asada!!";
-
+    // given names
+    if (yourNameInput == "Mr.Blank" && crushNameInput == "Falcon") {
+        percentage.innerHTML = `100%`
+        ageResult.innerHTML = `Age doesn't matter in this love story!`
+        phrase.innerHTML = `This must've been love at first sight...a perfect match that's meant to be. Don't forget to shine ✨`
     }
+
+
+    // final results
+    function finalResults() {
+        if (percentage < 11) {
+            phrase.innerHTML = "L, GIVE UP !!! it’s never going to happen!";
+    
+        } else if (10 < percentage < 21) {
+            phrase.innerHTML = "Find someone else they are not for u!";
+    
+        } else if (20 < percentage < 31) {
+            phrase.innerHTML = "You can try but I doubt it will happen";
+    
+        } else if (30 < percentage < 41) {
+            phrase.innerHTML = "Is your bank account cute? If no then no chance, if yes then maybe";
+    
+        } else if (40 < percentage < 51) {
+            phrase.innerHTML = "You're going to be the joke  of the friend group";
+    
+        } else if (50 < percentage < 61) {
+            phrase.innerHTML = "sure...";
+    
+        } else if (60 < percentage < 71) {
+            phrase.innerHTML = "If you're not scared of rejection, go for it but don't blame me if they don't accept";
+    
+        } else if (70 < percentage < 81) {
+            phrase.innerHTML = "Shoot your shot";
+    
+        } else if (80 < percentage < 91) {
+            phrase.innerHTML = "You better tell them";
+    
+        } else if (90 < percentage < 101) {
+            phrase.innerHTML = "Invite me to the carne asada!!";
+    
+        }
+    }
+    return finalResults();
 }
 
+calculateButton.addEventListener('click', compatibility);
