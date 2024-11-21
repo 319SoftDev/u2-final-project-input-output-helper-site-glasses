da3b997684e8fe64c3fcffbfaa8bce9711e6b4cd
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-21 10:38:22 -0500

    update html

--- script.js
+++ script.js
@@ -46,12 +46,6 @@ const compatibility = () => {
         ageResult.innerHTML = "You are the same age!";
     }
 
-    // compatibility with names
-    function getRandomInt() {
-        return Math.floor(Math.random() * 100);
-      }
-    percentage.innerHTML = `${getRandomInt()}%`;
-
     // given names
     if (yourNameInput == "Mr.Blank" && crushNameInput == "Falcon") {
         percentage.innerHTML = `100%`
@@ -61,40 +55,48 @@ const compatibility = () => {
 
 
     // final results
-    function finalResults() {
-        if (percentage < 11) {
-            phrase.innerHTML = "L, GIVE UP !!! it’s never going to happen!";
-    
-        } else if (10 < percentage < 21) {
-            phrase.innerHTML = "Find someone else they are not for u!";
-    
-        } else if (20 < percentage < 31) {
-            phrase.innerHTML = "You can try but I doubt it will happen";
-    
-        } else if (30 < percentage < 41) {
-            phrase.innerHTML = "Is your bank account cute? If no then no chance, if yes then maybe";
-    
-        } else if (40 < percentage < 51) {
-            phrase.innerHTML = "You're going to be the joke  of the friend group";
-    
-        } else if (50 < percentage < 61) {
-            phrase.innerHTML = "sure...";
-    
-        } else if (60 < percentage < 71) {
-            phrase.innerHTML = "If you're not scared of rejection, go for it but don't blame me if they don't accept";
-    
-        } else if (70 < percentage < 81) {
-            phrase.innerHTML = "Shoot your shot";
-    
-        } else if (80 < percentage < 91) {
-            phrase.innerHTML = "You better tell them";
-    
-        } else if (90 < percentage < 101) {
-            phrase.innerHTML = "Invite me to the carne asada!!";
-    
-        }
-    }
-    return finalResults();
+    // function finalResults() {
+
+    //     // compatibility with names
+    //     function getRandomInt() {
+    //         let num = Math.floor(Math.random() * 100);
+    //         if (num < 11) {
+    //             phrase.innerHTML = "L, GIVE UP !!! it’s never going to happen!";
+        
+    //         } else if (10 < num < 21) {
+    //             phrase.innerHTML = "Find someone else they are not for u!";
+        
+    //         } else if (20 < num < 31) {
+    //             phrase.innerHTML = "You can try but I doubt it will happen";
+        
+    //         } else if (30 < num < 41) {
+    //             phrase.innerHTML = "Is your bank account cute? If no then no chance, if yes then maybe";
+        
+    //         } else if (40 < num < 51) {
+    //             phrase.innerHTML = "You're going to be the joke  of the friend group";
+        
+    //         } else if (50 < num < 61) {
+    //             phrase.innerHTML = "sure...";
+        
+    //         } else if (60 < num < 71) {
+    //             phrase.innerHTML = "If you're not scared of rejection, go for it but don't blame me if they don't accept";
+        
+    //         } else if (70 < num < 81) {
+    //             phrase.innerHTML = "Shoot your shot";
+        
+    //         } else if (80 < num < 91) {
+    //             phrase.innerHTML = "You better tell them";
+        
+    //         } else if (90 < num < 101) {
+    //             phrase.innerHTML = "Invite me to the carne asada!!";
+        
+    //         }
+    //         return num
+    //     }
+    //     percentage.innerHTML = getRandomInt() + "%";
+    //     console.log(percentage)
+    // }
+    // return finalResults();
 }
 
 calculateButton.addEventListener('click', compatibility);
