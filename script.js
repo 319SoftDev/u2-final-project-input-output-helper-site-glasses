f07fb0af14bb9d834812a74e7bc5fce901506205
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-22 09:18:05 -0500

    update javascript

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ const yourNameInput = document.querySelector("#your-name");
 const crushNameInput = document.querySelector("#crush-name");
 const percentage = document.querySelector("#percentage");
 
-const nameCompatibility = (a, b) => {
+const nameCompatibility = () => {
     const letterVal = {A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10, K:11, L:12, M:13, N:14, O:15, P:16, Q:17, R:18, S:19, T:20, U:21, V:22, W:23, X:24, Y:25, Z:26}
     
     let sum1 = 0
     let sum2 = 0
 
-    for (let i = 0; i < a.length; i++) {
-        let letter1 = a[i].toUpperCase();
-        if (letterVal[letter1]) {
-            sum1 += letterVal[letter1];
+    for (let i = 0; i < yourNameInput.length; i++) {
+        let letter = yourNameInput[i].toUpperCase();
+        if (letterVal[letter]) {
+            sum1 += letterVal[letter];
+            console.log(sum1);
         }
     }
-    for (let i = 0; i < b.length; i++) {
-        let letter2 = b[i].toUpperCase();
-        if (letterVal[letter2]) {
-            sum2 += letterVal[letter2];
+    for (let i = 0; i < crushNameInput.length; i++) {
+        let letter = crushNameInput[i].toUpperCase();
+        if (letterVal[letter]) {
+            sum2 += letterVal[letter];
+            console.log(sum2);
         }
     }
     
-    let avgNum = (sum1 + sum2) / 2;
+    let avgNum = ((sum1 + sum2) / 2);
 
     percentage.innerHTML = `${avgNum}%`;
 }
-
-nameCompatibility(yourNameInput, crushNameInput);
 
 // age input functions
 const YourAgeInput = document.querySelector("#your-age");
@@ -80,4 +80,8 @@ const ageCompatibility = () => {
     // }
 }
 
-calculateButton.addEventListener('click', ageCompatibility);
+calculateButton.addEventListener('click', () => {
+    nameCompatibility();
+
+    ageCompatibility();
+});
