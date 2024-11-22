de137e621d7a852c7b164d9b9a2a8c5fd379c44c
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-22 10:39:34 -0500

    update javascript

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
 // name input functions
 const calculateButton = document.querySelector("#calculate-button");
+
 const yourNameInput = document.querySelector("#your-name");
 const crushNameInput = document.querySelector("#crush-name");
 const percentage = document.querySelector("#percentage");
 
 const nameCompatibility = () => {
     // assigning a number from 1 to 26 to each letter
-    const letterVal = {A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10, K:11, L:12, M:13, N:14, O:15, P:16, Q:17, R:18, S:19, T:20, U:21, V:22, W:23, X:24, Y:25, Z:26}
+    const letterVal = {A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10, K:11, L:12, M:13, N:14, O:15, P:16, Q:17, R:18, S:19, T:20, U:21, V:22, W:23, X:24, Y:25, Z:26};
     
     let sum1 = 0;
     console.log(sum1);
     let sum2 = 0;
     console.log(sum2);
 
-    for (let i = 0; i < yourNameInput.length; i++) {
-        let letter = yourNameInput[i].toUpperCase();
+    for (let i = 0; i < yourNameInput.value.length; i++) {
+        let letter = yourNameInput.value[i].toUpperCase();
         if (letterVal[letter]) {
             sum1 += letterVal[letter];
-            console.log(sum1);
         }
     }
-    for (let i = 0; i < crushNameInput.length; i++) {
-        let letter = crushNameInput[i].toUpperCase();
+
+    for (let i = 0; i < crushNameInput.value.length; i++) {
+        let letter = crushNameInput.value[i].toUpperCase();
         if (letterVal[letter]) {
             sum2 += letterVal[letter];
-            console.log(sum2);
         }
     }
     
-    let avgNum = ((sum1 + sum2) / 2);
+    let avgNum = (((sum1 * (yourNameInput.value.length + 1)) + (sum2 * (yourNameInput.value.length +1))) / 20);
+    console.log(avgNum);
 
     percentage.innerHTML = `${avgNum}%`;
 }
 
-// age input query selectors
-const YourAgeInput = document.querySelector("#your-age");
-const CrushAgeInput = document.querySelector("#crush-age");
-const ageResult = document.querySelector("#ageResults");
+// // age input query selectors
+// const YourAgeInput = document.querySelector("#your-age");
+// const CrushAgeInput = document.querySelector("#crush-age");
+// const ageResult = document.querySelector("#ageResults");
 
-// final results
-const phrase = document.querySelector("#phrase");
+// // final results
+// const phrase = document.querySelector("#phrase");
 
 
-const ageCompatibility = () => {
-    // compatibility with age
-    const yourAge = parseInt(YourAgeInput.value);
-    const crushAge = parseInt(CrushAgeInput.value);
+// const ageCompatibility = () => {
+//     // compatibility with age
+//     const yourAge = parseInt(YourAgeInput.value);
+//     const crushAge = parseInt(CrushAgeInput.value);
 
-    if (isNaN(yourAge) || isNaN(crushAge)) {
-        ageResult.innerHTML = "Please enter valid ages.";
-        return;
-    }
+//     if (isNaN(yourAge) || isNaN(crushAge)) {
+//         ageResult.innerHTML = "Please enter valid ages.";
+//         return;
+//     }
 
-    // Calculate the younger and older ranges based on your age
-    const youngerRange = 2 * (yourAge - 7);
-    const olderRange = (yourAge / 2) - 7;
-    console.log(youngerRange);
-    console.log(olderRange);
+//     // Calculate the younger and older ranges based on your age
+//     const youngerRange = 2 * (yourAge - 7);
+//     const olderRange = (yourAge / 2) - 7;
+//     console.log(youngerRange);
+//     console.log(olderRange);
 
-    // Compare ages and display result
-    if (yourAge < crushAge) {
-        if (crushAge > youngerRange) {
-            ageResult.innerHTML = "Too old!";
-        } else {
-            ageResult.innerHTML = "Just enough!";
-        }
-    } else if (yourAge > crushAge) {
-        if (crushAge < olderRange) {
-            ageResult.innerHTML = "Woah you creep too young...";
-        } else {
-            ageResult.innerHTML = "You're not too old for them (;";
-        }
-    } else {
-        ageResult.innerHTML = "You are the same age!";
-    }
+//     // Compare ages and display result
+//     if (yourAge < crushAge) {
+//         if (crushAge > youngerRange) {
+//             ageResult.innerHTML = "Too old!";
+//         } else {
+//             ageResult.innerHTML = "Just enough!";
+//         }
+//     } else if (yourAge > crushAge) {
+//         if (crushAge < olderRange) {
+//             ageResult.innerHTML = "Woah you creep too young...";
+//         } else {
+//             ageResult.innerHTML = "You're not too old for them (;";
+//         }
+//     } else {
+//         ageResult.innerHTML = "You are the same age!";
+//     }
 
     // // given names
     // if (yourNameInput == "Mr.Blank" && crushNameInput == "Falcon") {
@@ -81,6 +82,6 @@ const ageCompatibility = () => {
     //     ageResult.innerHTML = `Age doesn't matter in this love story!`
     //     phrase.innerHTML = `This must've been love at first sight...a perfect match that's meant to be. Don't forget to shine ✨`
     // }
-}
+// }
 
-calculateButton.addEventListener('click', ageCompatibility);
+calculateButton.addEventListener('click', nameCompatibility);
