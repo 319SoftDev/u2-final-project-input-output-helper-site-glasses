4d9b33faf9e923ba6da0eaae54d0763cc4193032
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-21 20:37:28 -0500

    update javascript

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const yourNameInput = document.querySelector("#your-name");
 const crushNameInput = document.querySelector("#crush-name");
 const percentage = document.querySelector("#percentage");
 
+const nameCompatibility = () => {
+    const letterVal = {A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10, K:11, L:12, M:13, N:14, O:15, P:16, Q:17, R:18, S:19, T:20, U:21, V:22, W:23, X:24, Y:25, Z:26}
+    
+    let sum1 = 0
+    let sum2 = 0
+
+    for (let i = 0; i < yourNameInput.length; i++) {
+        sum1 += yourNameInput.charCodeAt(i);
+    }
+    for (let i = 0; i < crushNameInput.length; i++) {
+        sum1 += crushNameInput.charCodeAt(i);
+    }
+
+    avgNum = (sum1 + sum2)/20;
+    percentage.innerHTML = `${avgNum}%`;
+}
+
 // age input functions
 const YourAgeInput = document.querySelector("#your-age");
 const CrushAgeInput = document.querySelector("#crush-age");
@@ -13,7 +30,7 @@ const ageResult = document.querySelector("#ageResults");
 const phrase = document.querySelector("#phrase")
 
 
-const compatibility = () => {
+const ageCompatibility = () => {
     // compatibility with age
     const yourAge = parseInt(YourAgeInput.value);
     const crushAge = parseInt(CrushAgeInput.value);
@@ -52,51 +69,6 @@ const compatibility = () => {
         ageResult.innerHTML = `Age doesn't matter in this love story!`
         phrase.innerHTML = `This must've been love at first sight...a perfect match that's meant to be. Don't forget to shine ✨`
     }
-
-
-    // final results
-    // function finalResults() {
-
-    //     // compatibility with names
-    //     function getRandomInt() {
-    //         let num = Math.floor(Math.random() * 100);
-    //         if (num < 11) {
-    //             phrase.innerHTML = "L, GIVE UP !!! it’s never going to happen!";
-        
-    //         } else if (10 < num < 21) {
-    //             phrase.innerHTML = "Find someone else they are not for u!";
-        
-    //         } else if (20 < num < 31) {
-    //             phrase.innerHTML = "You can try but I doubt it will happen";
-        
-    //         } else if (30 < num < 41) {
-    //             phrase.innerHTML = "Is your bank account cute? If no then no chance, if yes then maybe";
-        
-    //         } else if (40 < num < 51) {
-    //             phrase.innerHTML = "You're going to be the joke  of the friend group";
-        
-    //         } else if (50 < num < 61) {
-    //             phrase.innerHTML = "sure...";
-        
-    //         } else if (60 < num < 71) {
-    //             phrase.innerHTML = "If you're not scared of rejection, go for it but don't blame me if they don't accept";
-        
-    //         } else if (70 < num < 81) {
-    //             phrase.innerHTML = "Shoot your shot";
-        
-    //         } else if (80 < num < 91) {
-    //             phrase.innerHTML = "You better tell them";
-        
-    //         } else if (90 < num < 101) {
-    //             phrase.innerHTML = "Invite me to the carne asada!!";
-        
-    //         }
-    //         return num
-    //     }
-    //     percentage.innerHTML = getRandomInt() + "%";
-    //     console.log(percentage)
-    // }
-    // return finalResults();
 }
 
-calculateButton.addEventListener('click', compatibility);
+calculateButton.addEventListener('click', ageCompatibility);
