ee5d162ae576b9ac07b399c02001fe650017abd6
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-22 19:31:41 -0500

    update javascript

--- script.js
+++ script.js
@@ -10,14 +10,13 @@ const nameCompatibility = () => {
     const letterVal = {A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10, K:11, L:12, M:13, N:14, O:15, P:16, Q:17, R:18, S:19, T:20, U:21, V:22, W:23, X:24, Y:25, Z:26};
     
     let sum1 = 0;
-    console.log(sum1);
     let sum2 = 0;
-    console.log(sum2);
 
     for (let i = 0; i < yourNameInput.value.length; i++) {
         let letter = yourNameInput.value[i].toUpperCase();
         if (letterVal[letter]) {
             sum1 += letterVal[letter];
+            console.log(sum1);
         }
     }
 
@@ -25,6 +24,7 @@ const nameCompatibility = () => {
         let letter = crushNameInput.value[i].toUpperCase();
         if (letterVal[letter]) {
             sum2 += letterVal[letter];
+            console.log(sum2);
         }
     }
     
@@ -34,47 +34,47 @@ const nameCompatibility = () => {
     percentage.innerHTML = `${avgNum}%`;
 }
 
-// // age input query selectors
-// const YourAgeInput = document.querySelector("#your-age");
-// const CrushAgeInput = document.querySelector("#crush-age");
-// const ageResult = document.querySelector("#ageResults");
+// age input query selectors
+const YourAgeInput = document.querySelector("#your-age");
+const CrushAgeInput = document.querySelector("#crush-age");
+const ageResult = document.querySelector("#ageResults");
 
-// // final results
-// const phrase = document.querySelector("#phrase");
+// final results
+const phrase = document.querySelector("#phrase");
 
 
-// const ageCompatibility = () => {
-//     // compatibility with age
-//     const yourAge = parseInt(YourAgeInput.value);
-//     const crushAge = parseInt(CrushAgeInput.value);
+const ageCompatibility = () => {
+    // compatibility with age
+    const yourAge = parseInt(YourAgeInput.value);
+    const crushAge = parseInt(CrushAgeInput.value);
 
-//     if (isNaN(yourAge) || isNaN(crushAge)) {
-//         ageResult.innerHTML = "Please enter valid ages.";
-//         return;
-//     }
+    if (isNaN(yourAge) || isNaN(crushAge)) {
+        ageResult.innerHTML = "Please enter valid ages.";
+        return;
+    }
 
-//     // Calculate the younger and older ranges based on your age
-//     const youngerRange = 2 * (yourAge - 7);
-//     const olderRange = (yourAge / 2) - 7;
-//     console.log(youngerRange);
-//     console.log(olderRange);
+    // Calculate the younger and older ranges based on your age
+    const youngerRange = 2 * (yourAge - 7);
+    const olderRange = (yourAge / 2) - 7;
+    console.log(youngerRange);
+    console.log(olderRange);
 
-//     // Compare ages and display result
-//     if (yourAge < crushAge) {
-//         if (crushAge > youngerRange) {
-//             ageResult.innerHTML = "Too old!";
-//         } else {
-//             ageResult.innerHTML = "Just enough!";
-//         }
-//     } else if (yourAge > crushAge) {
-//         if (crushAge < olderRange) {
-//             ageResult.innerHTML = "Woah you creep too young...";
-//         } else {
-//             ageResult.innerHTML = "You're not too old for them (;";
-//         }
-//     } else {
-//         ageResult.innerHTML = "You are the same age!";
-//     }
+    // Compare ages and display result
+    if (yourAge < crushAge) {
+        if (crushAge > youngerRange) {
+            ageResult.innerHTML = "Too old!";
+        } else {
+            ageResult.innerHTML = "Just enough!";
+        }
+    } else if (yourAge > crushAge) {
+        if (crushAge < olderRange) {
+            ageResult.innerHTML = "Woah you creep too young...";
+        } else {
+            ageResult.innerHTML = "You're not too old for them (;";
+        }
+    } else {
+        ageResult.innerHTML = "You are the same age!";
+    }
 
     // // given names
     // if (yourNameInput == "Mr.Blank" && crushNameInput == "Falcon") {
@@ -82,6 +82,10 @@ const nameCompatibility = () => {
     //     ageResult.innerHTML = `Age doesn't matter in this love story!`
     //     phrase.innerHTML = `This must've been love at first sight...a perfect match that's meant to be. Don't forget to shine ✨`
     // }
-// }
+}
 
-calculateButton.addEventListener('click', nameCompatibility);
+calculateButton.addEventListener('click', () => {
+    nameCompatibility();
+
+    ageCompatibility();
+});
