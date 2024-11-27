b7c9b747b817cfc2bf6526e9bd63ef05ac10c309
FridaMarRamirez <frida.mar.ramirez@gmail.com>
2024-11-27 10:38:15 -0500

    add files

---
+++ prac.js
@@ -0,0 +1,10 @@
+// query selectors for zodaic compatibility
+const yourZodiac = document.querySelector("#your-zodiac");
+const crushZodiac = document.querySelector("#crush-zodiac");
+const submitButton = document.querySelector("#submit-button");
+
+const zodiacResult = document.querySelector("#zodiac-result");
+
+const zodiacCompatibility = () => {
+    
+}
